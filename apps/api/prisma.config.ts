ae3e36883143072aac63a112c5db4efaadae0954
lokman-hosen <lokman@mcc.com.bd>
2026-04-29 15:24:07 +0600

    Run issue fixed

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
+    seed: 'ts-node prisma/seed.ts', // ✅ ADD THIS
     // seed: {
     //   script: 'ts-node prisma/seed.ts',
     // },
